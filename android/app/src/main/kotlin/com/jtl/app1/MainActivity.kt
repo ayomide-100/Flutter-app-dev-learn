5137ad17426fe8609b52574cb68c0f570c5ef1db
ayomide-100 <166079576+ayomide-100@users.noreply.github.com>
2025-02-02 01:33:40 +0100

    Add files via upload

---
+++ android/app/src/main/kotlin/com/jtl/app1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jtl.app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()  => runApp(MaterialApp(
+  home: Scaffold(
+      appBar:AppBar(
+        title: Text('My First App'),
+        centerTitle: true, shadowColor: Colors.deepPurple,
+      ),
+    body:  Center(
+      child: Text('Hey Ayo'),
+    )
+      
+  ),
+)) ;
+
+
